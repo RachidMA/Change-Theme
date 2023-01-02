e0735e48a65cdb92ce0cade57ba4a891f7da5315
rachid <azzahir421@gmail.com>
2023-01-02 10:55:32 +0100

    Changing Webpage Theme

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$('#change-color').on('click',function(){
+    $text = $("button[id='change-color']").text()
+    if ($text === 'Dark Theme'){
+        $('body').css('background-color', ' rgb(202, 195, 195)')
+        $("button[id='change-color']").text('Light Theme')
+    }
+    else if ($text === 'Light Theme'){
+        $('body').css('background-color', 'white')
+        $("button[id='change-color']").text('Dark Theme')
+    }
+    
+});
